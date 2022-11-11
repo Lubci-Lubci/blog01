7351c9ce8665d6607673d2b6e7e013c87e34f377
Lubci-Lubci <lubo.han@gmail.com>
2022-11-11 13:54:56 +0100

    BloggerServiceImpl implements BloggerService

--- src/main/java/de/awacademy/blog/service/BloggerServiceImpl.java
+++ src/main/java/de/awacademy/blog/service/BloggerServiceImpl.java
@@ -1,10 +1,31 @@
 package de.awacademy.blog.service;
 
+import de.awacademy.blog.model.Blogger;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class BloggerServiceImpl implements {
+public class BloggerServiceImpl implements BloggerService {
 
 
+    @Override
+    public List<Blogger> getAllBloggers() {
+        return null;
+    }
 
+    @Override
+    public void saveBlogger(Blogger blogger) {
+
+    }
+
+    @Override
+    public Blogger getBloggerById(long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmployeeById(long id) {
+
+    }
 }
