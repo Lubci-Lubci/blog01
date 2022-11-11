f4465429e094f0066970d60b51099d9c2c2e1ae8
Lubci-Lubci <lubo.han@gmail.com>
2022-11-11 14:43:07 +0100

    bloggerController

--- src/main/java/de/awacademy/blog/controller/BloggerController.java
+++ src/main/java/de/awacademy/blog/controller/BloggerController.java
@@ -1,7 +1,33 @@
 package de.awacademy.blog.controller;
 
+import de.awacademy.blog.model.Blogger;
+import de.awacademy.blog.service.BloggerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BloggerController {
+
+    private BloggerService bloggerService;
+
+
+    @Autowired
+    public BloggerController(BloggerService bloggerService){
+        this.bloggerService = bloggerService;
+    }
+
+    @GetMapping("/bloggers")
+    public String bloggerHome(Model model){
+        model.addAttribute("listOfBloggers", bloggerService.getAllBloggers());
+
+        return "bloggers/index";
+    }
+
+
+
+
+
+
 }
