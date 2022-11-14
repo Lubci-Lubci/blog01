114573672ebf0ffc49a97b44a670a4aeef4c2cb6
Lubci-Lubci <lubo.han@gmail.com>
2022-11-14 10:47:09 +0100

    comment model, constructor, getters and setters

--- src/main/java/de/awacademy/blog/model/Comment.java
+++ src/main/java/de/awacademy/blog/model/Comment.java
@@ -2,6 +2,7 @@ package de.awacademy.blog.model;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.util.Date;
 
 @Entity
 @Table(name = "comments")
@@ -15,6 +16,33 @@ public class Comment {
     @Column(name = "comment_text")
     private String commentText;
 
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Date createdDate;
 
+    public Comment() {
+    }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCommentText() {
+        return commentText;
+    }
+
+    public void setCommentText(String commentText) {
+        this.commentText = commentText;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
 }
