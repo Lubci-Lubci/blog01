3a44cf0254a95f9bf2414c72e74b7ab33556a329
Lubci-Lubci <lubo.han@gmail.com>
2022-11-14 10:52:52 +0100

    comment repository

---
+++ src/main/java/de/awacademy/blog/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package de.awacademy.blog.repository;
+
+import de.awacademy.blog.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
